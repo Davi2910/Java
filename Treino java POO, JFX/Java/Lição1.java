5c85bb6fcfd5e8b5e869e6e65c95d3ab0c7a01a7
Davi Paulino Conceição <163527003+Davi2910@users.noreply.github.com>
2025-01-02 16:37:35 -0300

    Add files via upload

---
+++ Treino java POO, JFX/Java/Lição1.java
@@ -0,0 +1,19 @@
+package Java;
+// Lição  de fundametos tipos primmitivos.
+
+public class Lição1 {
+    public static void main(String[] args) {
+        String nome = "Davi Paulino Conceição";
+        int idade = 18;
+        char sexo = 'M';
+        double altura = 1.80;
+        boolean homem = true;
+        System.out.println("Nome:" + nome);
+        System.out.println("Idade:" + idade);
+        System.out.println("Sexo:" + sexo);
+        System.out.println("Altura:" + altura);
+        System.out.println("Homem:" + homem);
+        
+    }
+    
+}
